e46d0016ee93a96e2f842b98302ce353cefaf834
Vidmantas Uzkuraitis <v.uzkuraitis@yahoo.com>
2022-06-07 23:07:47 +0300

    created Notification component

---
+++ src/components/Notification/Notification.styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Notification = styled.div`
+  background: #eee;
+  border-radius: 0.5rem;
+  font-family: "Roboto", sans-serif;
+  padding: 1rem;
+`;
